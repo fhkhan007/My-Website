da586dd9b3293c3cf89d72e522edcddda247071e
MD: Fahad Hossain Khan <fahadhossainkhan@gmail.com>
2025-10-30 01:44:18 +0600

    modified some issue

--- 1-my-react-website/frontend/src/main.jsx
+++ 1-my-react-website/frontend/src/main.jsx
@@ -1,12 +1,10 @@
+import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
 
-import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router';
-
-
-createRoot(document.getElementById('root')).render(
- <BrowserRouter>
-   <App/>
- </BrowserRouter>
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
